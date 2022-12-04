b9e59a4cb874b631e1984182bd631f47a5c752ed
PAJohnson <tahumask@gmail.com>
2022-12-04 13:30:31 -0500

    Made templates folder with blank main.cpp

---
+++ day3/cpp/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <algorithm>
+
+void partOne() {
+    return;
+}
+
+void partTwo() {
+    return;
+}
+
+int main() {
+    std::ifstream file("../inputs/input.txt");
+    std::string str; 
+
+    while (std::getline(file, str))
+    {
+    }
+}
