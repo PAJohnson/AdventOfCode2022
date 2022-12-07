29fc9c650cf97d787135d09a45a71bb9ad3e59dd
PAJohnson <tahumask@gmail.com>
2022-12-06 21:40:46 -0500

    day 6 cpp cleanup

--- day6/cpp/main.cpp
+++ day6/cpp/main.cpp
@@ -12,31 +12,12 @@
 #define INPUT "../inputs/input.txt"
 #define SAMPLE "../inputs/sample.txt"
 
-// from https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
-// Neat!
-std::vector<std::string> split(const std::string str, const std::string regex_str)
-{
-    std::regex regexz(regex_str);
-    std::vector<std::string> list(std::sregex_token_iterator(str.begin(), str.end(), regexz, -1),
-                                  std::sregex_token_iterator());
-    return list;
-}
-
-void partOne() {
-    return;
-}
-
-void partTwo() {
-    return;
-}
-
 int main() {
     std::ifstream file(INPUT);
     std::string str; 
 
-    while (std::getline(file, str))
-    {
-    }
+    std::getline(file, str);
+
     // now str has entire input
     // part one - find index of first char following first unique 4 character sequence
     auto isUnique = [](std::string buff, int count, int index) {
@@ -46,14 +27,16 @@ int main() {
         }
         return subStr.size() == count;
     };
-    for (int i = 0; i < str.length() - 3; i++) {
+
+    for (int i = 0; i < str.length() - 4; i++) {
         if (isUnique(str, 4, i)) {
             std::cout << i+4 << std::endl;
             break;
         }
     }
+
     // part two - same as part one but looking for unique 14 character sequence
-    for (int i = 0; i < str.length() - 3; i++) {
+    for (int i = 0; i < str.length() - 14; i++) {
         if (isUnique(str, 14, i)) {
             std::cout << i+14 << std::endl;
             break;
